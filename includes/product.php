89624d2936cec28b4c2a3893605e97bb23a7c692
Kresimir <kresimir.banovic@gmail.com>
2013-06-16 12:06:12 +0200

    change

--- includes/product.php
+++ includes/product.php
@@ -3,14 +3,14 @@
 class Product extends databaseObject{
     public $id;
     public $name;
-    public $desc;
+    public $description;
     public $price;
     public $barcode;
     public $qty;
     public $category_id;
     
     public static $table_name = "product";
-    public static $table_fields = array("id","name","desc","price","barcode","qty","category_id");
+    public static $table_fields = array("id","name","description","price","barcode","qty","category_id");
     
     public static function findProductsByCatId($category_id){
         $sql = "SELECT * FROM " . static::$table_name . " WHERE category_id = " . $category_id;
